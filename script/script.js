8df68ee448915492f28c4aa116a85dc62ddd22e9
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-02-16 15:07:22 +0200

    initial commit

---
+++ script/script.js
@@ -0,0 +1,35 @@
+let audios;
+let keys;
+const className = 'active';
+const allowedKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+
+window.addEventListener('load', () => {
+	audios = document.querySelectorAll('.audio');
+	keys = document.querySelectorAll('.key');
+
+	keys.forEach((key, index) => {
+		key.addEventListener('click', () => {
+			audios[index].currentTime = 0;
+			audios[index].play();
+		});
+	});
+});
+
+document.addEventListener('keydown', e => {
+	if (!allowedKeys.includes(e.key.toUpperCase())) return;
+
+	const newAudios = Array.from(audios); // converts node list to array
+	const index = newAudios.findIndex(i => i.parentNode.innerText === e.key.toUpperCase());
+
+	handleClass(keys[index]);
+	audios[index].currentTime = 0;
+	audios[index].play();
+});
+
+const handleClass = el => {
+	el.classList.add(className);
+
+	el.addEventListener('animationend', () => {
+		el.classList.remove(className);
+	});
+};
